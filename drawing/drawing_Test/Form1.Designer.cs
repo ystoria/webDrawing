0b4a85327c71e10cc44a86869208e095ab8e2e7a
ystoria <122495944+ystoria@users.noreply.github.com>
2023-09-02 02:48:28 +0200

    Add files via upload

---
+++ drawing/drawing_Test/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace drawing_Test
+{
+    partial class frmDraw
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // frmDraw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmDraw";
+            this.Text = "Drawing";
+            this.Load += new System.EventHandler(this.frmDraw_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ drawing/drawing_Test/Form1.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Mehdi Calanducci
+ * 17.11.2021
+ * Perso
+ * Drawing Random Lines
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawing_Test
+{
+    public partial class frmDraw : Form
+    {
+        Random rand = new Random();
+        const int NB_LINES = 500;
+
+        void Artist()
+        {
+            using (var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height))
+            using (var gfx = Graphics.FromImage(bmp))
+            using (var draw = new Pen(Color.White))
+            {
+                //Draw the amount of lines specified in the const NB_LINES
+                gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                gfx.Clear(Color.Navy);
+                for (int i = 0; i < NB_LINES; i++)
+                {
+                    //Pick two random points inside the pictureBox Height and Width length
+                    var pt1 = new Point(rand.Next(bmp.Width), rand.Next(bmp.Height));
+                    var pt2 = new Point(rand.Next(bmp.Width), rand.Next(bmp.Height));
+                    gfx.DrawLine(draw, pt1, pt2);
+                }
+                //Copy the bitmap to the pictureBox
+                pictureBox1.Image?.Dispose();
+                pictureBox1.Image = (Bitmap)bmp.Clone();
+            }
+        }
+
+
+        public frmDraw()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDraw_Load(object sender, EventArgs e)
+        {
+            Artist();
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            Artist();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Artist();
+        }
+    }
+}
